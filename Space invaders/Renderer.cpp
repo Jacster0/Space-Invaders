1e5a718e764e68345406ea54b40a3e648f86e958
_Jacce_ <jacob.ogren2@gmail.com>
2020-12-29 11:53:01 +0100

    Updated the Renderer class

--- Space invaders/Renderer.cpp
+++ Space invaders/Renderer.cpp
@@ -7,27 +7,35 @@ Renderer::Renderer(Window& window, int index, uint32_t flags)
     :
     m_window(window)
 {
-   m_renderer = SDL_CreateRenderer(m_window.Get(), index, flags);
+   m_rendererSDL = SDL_CreateRenderer(m_window.GetSDLWindow(), index, flags);
 }
 
 Renderer::~Renderer()
 {
-    SDL_DestroyRenderer(m_renderer);
+    SDL_DestroyRenderer(m_rendererSDL);
 }
 
 void Renderer::Clear() {
-    SDL_RenderClear(m_renderer);
+    SDL_RenderClear(m_rendererSDL);
 }
 
 void Renderer::Present() {
-    SDL_RenderPresent(m_renderer);
+    SDL_RenderPresent(m_rendererSDL);
 }
 
 void Renderer::SetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
-    SDL_SetRenderDrawColor(m_renderer, r, g, b, a);
+    SDL_SetRenderDrawColor(m_rendererSDL, r, g, b, a);
 }
 
 void Renderer::DrawLine(const Point2D& start, const Point2D& end)
 {
-    SDL_RenderDrawLine(m_renderer, start.x, start.y, end.x, end.y);
+    SDL_RenderDrawLine(m_rendererSDL, start.x, start.y, end.x, end.y);
+}
+
+void Renderer::FillShape(CallBack fillCB) {
+    fillCB();
+}
+
+void Renderer::DrawOutLine(CallBack outLineCB) {
+    outLineCB();
 }
--- Space invaders/Renderer.h
+++ Space invaders/Renderer.h
@@ -1,20 +1,27 @@
 #pragma once
 #include <cstdint>
+#include <functional>
+
 //Wrapper class for SDL_Renderer
+enum RenderFlag { Fill = 1, OutLine = 2, Both = 3};
+using CallBack = std::function<void(void)>;
 class Window;
 class Point2D;
 class SDL_Renderer;
 class Renderer {
 public:
-    Renderer(Window& window, int index, uint32_t flags);
+    Renderer(Window& window, int index = -1, uint32_t flags = 0);
     ~Renderer();
     void Clear();
     void Present();
     void SetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
     void DrawLine(const Point2D& start, const Point2D& end);
+    void FillShape(CallBack fillCB);
+    void DrawOutLine(CallBack outLineCB);
+    SDL_Renderer* GetSDLRenderer() const { return m_rendererSDL; }
 private:
     //The window this renderer is drawing to
     Window& m_window;
     //the underlying sdl renderer
-    SDL_Renderer* m_renderer;
+    SDL_Renderer* m_rendererSDL;
 };
