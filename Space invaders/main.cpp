ca1bbc30989e51acc122dd023570f56d5b44201f
_Jacce_ <jacob.ogren2@gmail.com>
2020-12-29 11:57:46 +0100

    Updated main

--- Space invaders/main.cpp
+++ Space invaders/main.cpp
@@ -1,23 +1,9 @@
 #include "SDL.h"
-#include "Window.h"
-#include "Renderer.h"
+#include "Application.h"
+#include <memory>
 
 int main(int argc, char** argv) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    Window* wnd = Window::Create();
-    wnd->Show();
-
-    {
-        Renderer renderer(*wnd, -1, 0);
-        renderer.SetColor(0, 255, 0, 255);
-        renderer.Clear();
-        renderer.Present();
-       
-        SDL_Delay(50000);
-    }
-
-    Window::Destroy();
-    SDL_Quit();
-    return 0;
+    Application app;
+    app.Run();
+    return 0;         
 }
