e650e8f1018855dee010391fe44e36a36c8d9768
_Jacce_ <jacob.ogren2@gmail.com>
2020-12-31 16:27:39 +0100

    Have a fixed size of the screen

--- Space invaders/Window.cpp
+++ Space invaders/Window.cpp
@@ -3,11 +3,11 @@
 
 Window* Window::m_instance = nullptr;
 
-Window* Window::Create(int width, int height) {
+Window* Window::Create() {
     if (m_instance) {
         return m_instance;
     }
-    m_instance = new Window(width, height);
+    m_instance = new Window(m_width, m_height);
     return m_instance;
 }
 
@@ -21,10 +21,6 @@ void Window::Show() noexcept {
     SDL_ShowWindow(m_windowSDL);
 }
 
-Window::Window(int width, int height)
-    :
-    m_width(width),
-    m_height(height)
-{
+Window::Window(int width, int height) {
     m_windowSDL = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_width, m_height, SDL_WINDOW_HIDDEN);
 }
