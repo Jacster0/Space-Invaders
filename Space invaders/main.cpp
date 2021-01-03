e7827b107b73f4e29313600ad73b8345b7e03c62
_Jacce_ <jacob.ogren2@gmail.com>
2021-01-03 23:29:26 +0100

    Capture exceptions in the main function. If we are on a windows machine we also do some debug error printing to the output window

--- Space invaders/main.cpp
+++ Space invaders/main.cpp
@@ -2,8 +2,26 @@
 #include "Game.h"
 #include <memory>
 
+#ifdef  _WIN32
+#include <windows.h>
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+
 int main(int argc, char** argv) {
-    Game game;
-    game.Run();
-    return 0;         
+    try {
+        return Game{}.Run();
+    }
+    catch (const std::exception& e) {
+#ifdef _WIN32
+        char buffer[512];
+        sprintf_s(buffer, "Error message: %s\n", e.what());
+        OutputDebugStringA(buffer);
+#endif
+   }
+    catch (...) {
+#ifdef _WIN32
+        OutputDebugStringW(L"Error message: Unknown error\n");
+#endif
+    }
 }
